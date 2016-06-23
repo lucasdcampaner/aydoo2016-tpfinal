bc4ca5c6ce6c6859d28e23a1f95ba52f14733e88
Lucas <lucasdcampaner@gmail.com>
2016-06-22 23:31:25 -0300

    Primera version de la clase Nave.

---
+++ model/Nave.rb
@@ -0,0 +1,19 @@
+class Nave
+
+    @vida
+    @masa
+    
+    def initialize(vida, masa)     
+        @vida = vida    
+        @masa = masa
+    end
+
+    def vida
+        @vida
+    end
+
+    def masa
+        @masa
+    end     
+
+end
