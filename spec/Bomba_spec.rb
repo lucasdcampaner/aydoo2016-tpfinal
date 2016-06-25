65b4cea1a203565d37933def8d3df2fda45a30a6
Lucas <lucasdcampaner@gmail.com>
2016-06-25 11:33:35 -0300

    Cambios para medir cobertura.

--- spec/Bomba_spec.rb
+++ spec/Bomba_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec' 
+require 'spec_helper'
 require_relative '../model/Bomba'
 
 describe 'Bomba' do
--- spec/Integracion_spec.rb
+++ spec/Integracion_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec' 
+require 'spec_helper'
 
 describe 'Distintos tipos de choque' do
 
