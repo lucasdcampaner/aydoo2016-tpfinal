2d917c9349bcfd6235ee8976d4aa2b4cba544f74
Lucas <lucasdcampaner@gmail.com>
2016-06-26 18:26:32 -0300

    Correccion de nombre de variable en clase Excepciones_spec.

--- spec/Excepciones_spec.rb
+++ spec/Excepciones_spec.rb
@@ -7,31 +7,31 @@ describe 'Excepciones' do
   it 'Deberia lanzar excepcion cuando el objeto que choca esta muerto' do
     
     objeto_espacial_que_choca = ObjetoEspacial.new() 
-    objeto_espacial_que_chocado = ObjetoEspacial.new() 
+    objeto_espacial_chocadodo = ObjetoEspacial.new() 
     vida = 0
     objeto_espacial_que_choca.vida = vida
 
-    expect{objeto_espacial_que_choca.chocar(objeto_espacial_que_chocado)}.to raise_exception(ObjetoEspacialMuertoException)
+    expect{objeto_espacial_que_choca.chocar(objeto_espacial_chocadodo)}.to raise_exception(ObjetoEspacialMuertoException)
   end
 
   it 'Deberia lanzar excepcion cuando el objeto que es chocado esta muerto' do
     
     objeto_espacial_que_choca = ObjetoEspacial.new() 
-    objeto_espacial_que_chocado = ObjetoEspacial.new() 
+    objeto_espacial_chocadodo = ObjetoEspacial.new() 
     vida = 0
-    objeto_espacial_que_chocado.vida = vida
+    objeto_espacial_chocadodo.vida = vida
 
-    expect{objeto_espacial_que_choca.chocar(objeto_espacial_que_chocado)}.to raise_exception(ObjetoEspacialMuertoException)
+    expect{objeto_espacial_que_choca.chocar(objeto_espacial_chocadodo)}.to raise_exception(ObjetoEspacialMuertoException)
   end
 
   it 'Deberia lanzar excepcion cuando ambos objetos estan muertos' do
     
     objeto_espacial_que_choca = ObjetoEspacial.new() 
-    objeto_espacial_que_chocado = ObjetoEspacial.new() 
+    objeto_espacial_chocadodo = ObjetoEspacial.new() 
     vida = 0
     objeto_espacial_que_choca.vida = vida    
-    objeto_espacial_que_chocado.vida = vida
+    objeto_espacial_chocadodo.vida = vida
 
-    expect{objeto_espacial_que_choca.chocar(objeto_espacial_que_chocado)}.to raise_exception(ObjetoEspacialMuertoException)
+    expect{objeto_espacial_que_choca.chocar(objeto_espacial_chocadodo)}.to raise_exception(ObjetoEspacialMuertoException)
   end
 end
