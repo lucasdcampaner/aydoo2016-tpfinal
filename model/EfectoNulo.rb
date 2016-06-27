92b3729f4641a9a2ebab2cd08ceacecd787cd196
Lucas <lucasdcampaner@gmail.com>
2016-06-27 00:56:38 -0300

    Comentario en EfectoNulo.

--- model/EfectoNulo.rb
+++ model/EfectoNulo.rb
@@ -1,4 +1,5 @@
 require_relative '../model/Efecto'
 
-class EfectoNulo < Efecto
+class EfectoNulo < Efecto	
+	#No tiene estado ni comportamiento, pero existe para agregar claridad al codigo respecto al dominio
 end
