83881cac2798fcf7fe82a5bcd6a60da9de04c452
lcampaner <lucas.campaner@grupoesfera.com.ar>
2016-06-27 14:34:53 -0300

    Comentario en Efectos.

--- model/Efecto.rb
+++ model/Efecto.rb
@@ -1,5 +1,7 @@
 class Efecto
-  #No tiene estado ni comportamiento, pero existe para agregar claridad al codigo respecto al dominio
+
+#No tiene estado ni comportamiento, pero existe para agregar claridad al codigo respecto al dominio
+  
   def afectar_objeto(objeto_espacial_que_choca, objeto_espacial_chocado)
   end
 end
--- model/EfectoNulo.rb
+++ model/EfectoNulo.rb
@@ -1,5 +1,6 @@
 require_relative '../model/Efecto'
+
 #No tiene estado ni comportamiento, pero existe para agregar claridad al codigo respecto al dominio
+
 class EfectoNulo < Efecto	
-	
 end
